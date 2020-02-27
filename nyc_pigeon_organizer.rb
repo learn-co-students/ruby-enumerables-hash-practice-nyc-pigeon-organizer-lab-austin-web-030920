e35d31e0460ee0ab36bf4721824721339d4d11a8
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-27 17:21:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,46 +1,24 @@
 require 'pp'
+require 'pry'
+
 
 def nyc_pigeon_organizer(data)
-pigeon_by_name = {}
-pigeon_name_array = list_of_names(data)
-
-pigeon_by_name=Hash[pigeon_name_array.map {|key, value|[key, value]}]
-pigeon_by_name
+new = {}
+data.map do |key, value| 
+  # value #=> {:purple=>["Theo", "Peter Jr.", "Lucky"], etc...
+  value.map do |n_value, names| 
+    # names #=> ["Theo", "Peter Jr.", "Lucky"]
+    names.map do |name| 
+      # name #=> "Theo"
+      if !new[name]
+        new[name] = {}
+      end
+      if !new[name][key]
+        new[name][key] = []
+      end
+      new[name][key] << n_value.to_s
+    end
+  end
 end
-
-def list_of_names(hash)
-names = []
-names = hash[:gender].values_at(:male, :female)
-names.flatten
+new
 end
-
-def assign_color (hash)
-  color = {}
-end
-
-def assign_gender(hash)
-  gender = {}
-end
-
-def assign_lives(hash)
-   lives = {}
-end
-
-
-
-
-### data being passed through
-# pigeon_data = {
-#   :color => {
-#     :brown => ["Luca"],
-#     :black => ["Lola"],
-#   },
-#   :gender => {
-#     :male => ["Luca"],
-#     :female => ["Lola"]
-#   },
-#   :lives => {
-#     "Central Park" => ["Lola"],
-#     "Library" => ["Luca"]
-#   }
-# }
