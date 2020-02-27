19757df8fa6eab325026248d00bd3c6e3c1ab57f
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-27 02:37:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,46 @@
+require 'pp'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_by_name = {}
+pigeon_name_array = list_of_names(data)
+
+pigeon_by_name=Hash[pigeon_name_array.map {|key, value|[key, value]}]
+pigeon_by_name
 end
+
+def list_of_names(hash)
+names = []
+names = hash[:gender].values_at(:male, :female)
+names.flatten
+end
+
+def assign_color (hash)
+  color = {}
+end
+
+def assign_gender(hash)
+  gender = {}
+end
+
+def assign_lives(hash)
+   lives = {}
+end
+
+
+
+
+### data being passed through
+# pigeon_data = {
+#   :color => {
+#     :brown => ["Luca"],
+#     :black => ["Lola"],
+#   },
+#   :gender => {
+#     :male => ["Luca"],
+#     :female => ["Lola"]
+#   },
+#   :lives => {
+#     "Central Park" => ["Lola"],
+#     "Library" => ["Luca"]
+#   }
+# }
