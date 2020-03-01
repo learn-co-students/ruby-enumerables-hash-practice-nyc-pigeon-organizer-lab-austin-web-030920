aea64ec1b6665c2e94456d54b0b43a5c530309c6
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 04:59:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  
+  #att = "attribute" (so here attkey means like color/gender/lives and attvalue is the value)
+  
+  data.each do |attkey, attvalue|
+    attvalue.each do |call, names|
+      names.each do |name|
+        pigeons[name] ||= {}
+        pigeons[name][attkey] ||= []
+        pigeons[name][attkey].push(call.to_s)
+ 
+  #to_s is basically turning the integers into strings! 
+  
+      end
+    end
+  end
+  pigeons
 end
