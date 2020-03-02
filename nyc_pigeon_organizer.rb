639f831d2e793d016755708930832a20a2d18722
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 04:47:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  
+  data.each { |descriptions, item|
+    item.each { |results, names|
+      names.each { |name|
+      
+        if pigeons[name] == nil
+          pigeons[name] = {}
+        end
+        if pigeons[name][descriptions] == nil
+          pigeons[name][descriptions] = []
+        end
+        final = results.to_s
+        pigeons[name][descriptions].push(final)
+      }
+    }
+  }
+  
+  return pigeons
+  
 end
