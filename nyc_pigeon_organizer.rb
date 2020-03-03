5c0f77aeb359b3ef362ab8eb4467284a3660e488
l.jackson96 <l.jackson96@outlook.com>
2020-03-03 09:34:05 -0600

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
+  #binding.pry
   # write your code here!
+  pigeon_list = {}
+
+  data.each do |key, value|
+    value.each do |new, names|
+      names.each do |name|
+
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+
+        if !pigeon_list[name][key]
+          pigeon_list[name][key] = []
+        end
+
+        pigeon_list[name][key] << new.to_s
+      end
+    end
+  end
+  pigeon_list
 end
